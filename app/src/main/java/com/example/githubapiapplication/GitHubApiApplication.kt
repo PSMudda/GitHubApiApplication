aaf27861c0e5b469b2c2ce4f23ece3a209d476ca
psmudda <pritamhonwad@gmail.com>
2022-02-24 18:20:06 -0500

    Code refactor

--- app/src/main/java/com/example/githubapiapplication/GitHubApiApplication.kt
+++ app/src/main/java/com/example/githubapiapplication/GitHubApiApplication.kt
@@ -1,7 +1,6 @@
 package com.example.githubapiapplication
 
 import android.app.Application
-import android.content.Context
 import com.example.githubapiapplication.di.APIComponent
 import com.example.githubapiapplication.di.DaggerAPIComponent
 
@@ -9,25 +8,18 @@ import com.example.githubapiapplication.di.DaggerAPIComponent
 class GitHubApiApplication : Application() {
 
     companion object {
-       // var ctx: Context? = null
         lateinit var apiComponent: APIComponent
-
     }
-
 
     override fun onCreate() {
         super.onCreate()
-        apiComponent=initDaggerComponent()
+        initDaggerComponent()
     }
 
-    private fun initDaggerComponent():APIComponent {
+    private fun initDaggerComponent() {
         apiComponent = DaggerAPIComponent
             .builder()
             .build()
-        return apiComponent
     }
 
-    fun getAPIComponentInstance():APIComponent{
-        return apiComponent
-    }
 }
